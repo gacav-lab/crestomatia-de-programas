bc3d12098c38b3dd9729e64cad387ea0f4169699
Gabriel Cavalcante de Jesus Oliveira <94535032+gacav-lab@users.noreply.github.com>
2022-10-27 19:01:29 -0300

    :tada: feat: commit

--- statistic.py
+++ statistic.py
@@ -1,3 +1,32 @@
 #!/usr/bin/env python3
 
-#TODO
+# INCOMPLETO
+
+#median=0
+#mode=0
+
+total_people = int(input("Total de pessoas: "))
+age = int(input("Idade da pessoa 1: "))
+
+age_accumulator = age
+age_of_the_oldest_person = age
+age_of_the_youngest_person = age
+
+for counter in range(1, total_people + 1):
+    age = int(input("Idade da pessoa 1: "))
+
+    age_accumulator += age;
+
+    if(age < age_of_the_youngest_person):
+        age_of_the_youngest_person = age
+
+    if(age > age_of_the_oldest_person):
+        age_of_the_oldest_person = age
+
+mean = age_accumulator / total_people;
+
+#print("Moda das idades:", mode)
+print("Media das idades:", mean)
+#print("Mediana das idades:", median)
+print("Idade da pessoa mais velha:", age_of_the_oldest_person)
+print("Idade da pessoa mais nova:", age_of_the_youngest_person)
