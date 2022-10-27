b2c09e5df5e3cbaa9f865553585b856b751e4064
Gabriel Cavalcante de Jesus Oliveira <94535032+gacav-lab@users.noreply.github.com>
2022-10-27 20:25:38 -0300

    :tada: feat: commit

--- statistic.sh
+++ statistic.sh
@@ -1,3 +1,20 @@
 #!/usr/bin/env bash
 
-#TODO
+swap() {
+    ages[i]=$((ages[i] + ages[j]))
+    ages[j]=$((ages[i] - ages[j]))
+    ages[i]=$((ages[i] - ages[j]))
+}
+
+bubble_sort() {
+  local i
+  local j
+  
+  for i in $(seq 0 $total_people): do
+    for j in $(seq 0 $total_people); do
+      if [ ages[i] > ages[j] ]; then
+        swap
+      fi
+    done
+  done
+}
