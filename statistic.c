c9c62e1ee394eee732710c0ce7f518e6bc6fd3b7
gabrielcavalcante-cs <gabriel.lcifba@gmail.com>
2022-10-28 11:19:15 -0300

    :wrench: feat: commit

--- statistic.c
+++ statistic.c
@@ -3,11 +3,12 @@
 
 void swap(int *ages, int i, int j);
 void bubble_sort(int *ages, int total_people);
+int f_median(int total_people, int *ages);
 
 int main(int argc, char **argv) {
-    int* ages = NULL, onces = NULL;
+    int *ages = NULL;
     int age_of_the_oldest_person, age_of_the_youngest_person;
-    int half, total_people, age_accumulator, most_frequently, mean, mode, median, age;
+    int half, total_people, age_accumulator, mean, median, age;
 
 	printf("Total de pessoas: ");
 	scanf("%d", &total_people);
@@ -19,18 +20,11 @@ int main(int argc, char **argv) {
 	age_of_the_oldest_person = age;
 	age_of_the_youngest_person = age;
 	ages = malloc(total_people * sizeof(int));
-	onces = malloc(total_people * sizeof(int));
 	*ages = age;
-
-    for(int i = 0; i < total_people; i++) {
-        *(onces + i) = 0;
-    }
 
 	for(int i = 1; i < total_people; i++) {
 		printf("Idade da pessoa %d: ", i + 1);
 		scanf("%d", &age);
-
-		age_accumulator += age;
 
 		if(age < age_of_the_youngest_person) {
 			age_of_the_youngest_person = age;
@@ -40,40 +34,13 @@ int main(int argc, char **argv) {
 			age_of_the_oldest_person = age;
 		}
 		
+		age_accumulator += age;
 		*(ages + i) = age;
 	}
 
-    bubble_sort(&ages[0], total_people);
-
 	mean = age_accumulator / total_people;
-	half = total_people / 2;
-	half--;
-	
-	if(total_people % 2 != 0) {
-	    median = total_people / 2 + 1;
-	} else {
-	    median = (*(ages + half) + *(ages + half + 1)) / 2;
-	}
+	median = f_median(total_people, &ages[0]);
 
-    for(int i = 0; i < total_people; i++) {
-        for(int j = 0; j < total_people; j++) {
-            if(*(ages + j) == *(ages + i)) {
-                onces[i]++;
-            }
-        }
-    }
-
-    most_frequently = *onces;
-
-    for(int i = 1; i < total_people; i++) {
-        if(*(onces + i) > most_frequently) {
-            most_frequently = i;
-        }
-    }
-
-    mode = *(ages + most_frequently);
-
-	printf("Moda das idades: %d\n", mode);
 	printf("Media das idades: %d\n", mean);
 	printf("Mediana das idades: %d\n", median);
 	printf("Idade da pessoa mais velha: %d\n", age_of_the_oldest_person);
@@ -96,4 +63,19 @@ void bubble_sort(int *ages, int total_people) {
             }
         }
     }
+}
+
+int f_median(int total_people, int *ages) {
+	int half;
+
+	bubble_sort(&ages[0], total_people);
+
+	half = total_people / 2;
+
+	if(total_people % 2 != 0) {
+	    return *(ages + half);
+	} else {
+		half--;
+	    return (*(ages + half) + *(ages + half + 1)) / 2;
+	}
 }
